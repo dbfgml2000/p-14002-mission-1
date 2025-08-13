a726d67518199862c7bc7df369c9a295059b623b
dbfgml20 <dbfgml20@gmail.com>
2025-08-13 12:48:09 +0900

    12, 13강 - PostDto, PostWithContentDto, PostCommentDto 변환

--- backend/src/main/java/com/back/domain/post/postComment/dto/PostCommentDto.kt
+++ backend/src/main/java/com/back/domain/post/postComment/dto/PostCommentDto.kt
@@ -3,7 +3,7 @@ package com.back.domain.post.postComment.dto
 import com.back.domain.post.postComment.entity.PostComment
 import java.time.LocalDateTime
 
-class PostCommentDto private constructor( // private을 걸어 해당 생성자 직접 호출 못하게 함
+data class PostCommentDto (
     val id: Int,
     val createDate: LocalDateTime,
     val modifyDate: LocalDateTime,
