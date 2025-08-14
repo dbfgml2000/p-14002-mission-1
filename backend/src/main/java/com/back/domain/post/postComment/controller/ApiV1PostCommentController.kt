3af519c69077100ca197ca8b1e168fab3690a663
dbfgml20 <dbfgml20@gmail.com>
2025-08-14 23:57:19 +0900

    PostCommentController 변환

---
+++ backend/src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.kt
@@ -0,0 +1,89 @@
+package com.back.domain.post.postComment.controller
+
+import com.back.domain.post.post.service.PostService
+import com.back.domain.post.postComment.dto.PostCommentDto
+import com.back.global.rq.Rq
+import com.back.global.rsData.RsData
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/v1/posts/{postId}/comments")
+@Tag(name = "ApiV1PostCommentController", description = "API 댓글 컨트롤러")
+@SecurityRequirement(name = "bearerAuth")
+class ApiV1PostCommentController(
+    private val postService: PostService,
+    private val rq: Rq
+) {
+
+    @GetMapping
+    @Transactional(readOnly = true)
+    @Operation(summary = "다건 조회")
+    fun getItems(@PathVariable postId: Int): List<PostCommentDto> {
+        val post = postService.findById(postId) ?: throw IllegalArgumentException("존재하지 않는 글입니다.")
+        return post.comments.map { PostCommentDto(it) }
+    }
+
+    @GetMapping("/{id}")
+    @Transactional(readOnly = true)
+    @Operation(summary = "단건 조회")
+    fun getItem(@PathVariable postId: Int, @PathVariable id: Int): PostCommentDto {
+        val post = postService.findById(postId) ?: throw IllegalArgumentException("존재하지 않는 글입니다.")
+        val comment = post.findCommentById(id) ?: throw IllegalArgumentException("존재하지 않는 댓글입니다.")
+        return PostCommentDto(comment)
+    }
+
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    @Operation(summary = "삭제")
+    fun delete(@PathVariable postId: Int, @PathVariable id: Int): RsData<Void> {
+        val actor = rq.actor
+        val post = postService.findById(postId) ?: throw IllegalArgumentException("존재하지 않는 글입니다.")
+        val comment = post.findCommentById(id) ?: throw IllegalArgumentException("존재하지 않는 댓글입니다.")
+
+        comment.checkActorCanDelete(actor)
+        postService.deleteComment(post, comment)
+
+        return RsData("200-1", "${id}번 댓글이 삭제되었습니다.")
+    }
+
+
+    data class PostCommentModifyReqBody(val content: @NotBlank @Size(min = 2, max = 100) String)
+
+    @PutMapping("/{id}")
+    @Transactional
+    @Operation(summary = "수정")
+    fun modify(@PathVariable postId: Int, @PathVariable id: Int, @RequestBody @Valid reqBody: PostCommentModifyReqBody): RsData<Void> {
+        val actor = rq.actor
+        val post = postService.findById(postId) ?: throw IllegalArgumentException("존재하지 않는 글입니다.")
+        val comment = post.findCommentById(id) ?: throw IllegalArgumentException("존재하지 않는 댓글입니다.")
+
+        comment.checkActorCanModify(actor)
+        postService.modifyComment(comment, reqBody.content)
+
+        return RsData("200-1", "${id}번 댓글이 수정되었습니다.")
+    }
+
+
+    data class PostCommentWriteReqBody(val content: @NotBlank @Size(min = 2, max = 100) String)
+
+    @PostMapping
+    @Transactional
+    @Operation(summary = "작성")
+    fun write(@PathVariable postId: Int, @RequestBody @Valid reqBody: PostCommentWriteReqBody): RsData<PostCommentDto> {
+        val actor = rq.actor
+        val post = postService.findById(postId) ?: throw IllegalArgumentException("존재하지 않는 글입니다.")
+
+        val comment = postService.writeComment(actor, post, reqBody.content)
+        postService.flush()
+
+        return RsData("201-1", "${comment.id}번 댓글이 작성되었습니다.", PostCommentDto(comment))
+    }
+}
